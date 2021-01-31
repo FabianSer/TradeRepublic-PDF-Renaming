7764e8fafa6d3291082c5dc8aa309f5eb1a094fb
C. Wu <contact@thedrwu.com>
2021-01-31 22:53:55 +0100

    Handles PDF without ISIN

--- renamer.py
+++ renamer.py
@@ -10,10 +10,15 @@ def get_naming(file_name):
     pdfReader = PdfFileReader(pdfFile)
     pdfContent = pdfReader.getPage(0).extractText()
     pdfFile.close()
-    ISIN = re.search(r"(?<=ISIN: ).*?(?=\s)", pdfContent)[0]
+    ISIN = re.search(r"(?<=ISIN: ).*?(?=\s)", pdfContent)
     DATUM = re.search(r"(?<=DATUM\n).*?(?=\n)", pdfContent)[0]
     DATUM_formatted = DATUM[-4:]+DATUM[3:5]+DATUM[:2]
-    return DATUM_formatted+'_'+ISIN+'.pdf'
+    if not ISIN:
+        i = 1
+        while os.path.exists(DATUM_formatted+'_'+str(i)+'.pdf'):
+            i += 1
+        return DATUM_formatted+'_'+str(i)+'.pdf'
+    return DATUM_formatted+'_'+ISIN[0]+'.pdf'
 
 
 all_files = (os.listdir('./input/'))
